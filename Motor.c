e4132c5ef1a9dde3349aa393a9c0f6946e77ca1c
Gavin Chua <156627779+parasytezz@users.noreply.github.com>
2025-03-25 15:33:20 +0800

    Update Motor.c

--- Motor.c
+++ Motor.c
@@ -1,118 +1,100 @@
 #include "MKL25Z4.h"
 
-#define LEFT_FORWARD 0 // PTB0 TPM1_CH0 (M1 & M3)
-#define LEFT_BACK 1 // PTB1 TPM1_CH1 (M1 & M3)
-#define RIGHT_FORWARD 2 // PTB2 TPM2_CH0 (M2 & M4)
-#define RIGHT_BACK 3 //PTB3 TPM2_CH1 (M2 & M4)
-#define DUTY_CYCLE 7500 // (50hz)
+// Motor control pin definitions (renamed for clarity)
+#define L_BACK_FW      0   // PTB0  - TPM1_CH0
+#define L_BACK_BW      1   // PTB1  - TPM1_CH1
+#define R_BACK_FW      2   // PTB2  - TPM2_CH0
+#define R_BACK_BW      3   // PTB3  - TPM2_CH1
+#define L_FRONT_FW     10  // PTB10 - TPM0_CH1
+#define L_FRONT_BW     11  // PTB11 - TPM0_CH0
+#define R_FRONT_FW     12  // PTB12 - TPM1_CH0 (Alt location)
+#define R_FRONT_BW     13  // PTB13 - TPM1_CH1 (Alt location)
 
-volatile int speed = 2; // Control turning speed to be slower
+#define MASK(x) (1 << (x))
+#define DUTY_CYCLE 7500
+
+volatile int speed = 2;
 
 void initPWM(void) {
-    // enable clock gating for PORTB
+    // Enable clock gating for Port B
     SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+    
+    // Set pins to TPM alternate function (ALT 3)
+    PORTB->PCR[L_BACK_FW]     = PORT_PCR_MUX(3);
+    PORTB->PCR[L_BACK_BW]     = PORT_PCR_MUX(3);
+    PORTB->PCR[R_BACK_FW]     = PORT_PCR_MUX(3);
+    PORTB->PCR[R_BACK_BW]     = PORT_PCR_MUX(3);
+    PORTB->PCR[L_FRONT_FW]    = PORT_PCR_MUX(3);
+    PORTB->PCR[L_FRONT_BW]    = PORT_PCR_MUX(3);
+    PORTB->PCR[R_FRONT_FW]    = PORT_PCR_MUX(3);
+    PORTB->PCR[R_FRONT_BW]    = PORT_PCR_MUX(3);
 
-    // configure mode 3 for the PWM pin operation
-    // For left motors
-    PORTB->PCR[LEFT_FORWARD] &= ~PORT_PCR_MUX_MASK;
-    PORTB->PCR[LEFT_FORWARD] |= PORT_PCR_MUX(3);
-    PORTB->PCR[LEFT_BACK] &= ~PORT_PCR_MUX_MASK;
-    PORTB->PCR[LEFT_BACK] |= PORT_PCR_MUX(3);
+    // Enable TPM0, TPM1, TPM2
+    SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK | SIM_SCGC6_TPM1_MASK | SIM_SCGC6_TPM2_MASK;
 
-    // For right motors
-    PORTB->PCR[RIGHT_FORWARD] &= ~PORT_PCR_MUX_MASK;
-    PORTB->PCR[RIGHT_FORWARD] |= PORT_PCR_MUX(3);
-    PORTB->PCR[RIGHT_BACK] &= ~PORT_PCR_MUX_MASK;
-    PORTB->PCR[RIGHT_BACK] |= PORT_PCR_MUX(3);
-
-    // enable clock gating for Timer 1
-    SIM->SCGC6 = (SIM_SCGC6_TPM1_MASK) | (SIM_SCGC6_TPM2_MASK);
-
-    // select clock for TPM module
+    // Set TPM clock source
     SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
-    SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); // MCGFLLCLK clock or MCGPLLCLK/2 clock
+    SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
 
-    // Set Mod value 48000000 / 120 = 375000 / 7500 = 50hz
+    // Set MOD value for all TPMs
+    TPM0->MOD = DUTY_CYCLE;
     TPM1->MOD = DUTY_CYCLE;
     TPM2->MOD = DUTY_CYCLE;
 
-    // Edged aligned PWM
-    // upate snc register: CMOD = 01, PS = 111 (128)
-    TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
-    TPM1->SC |= (TPM_SC_CMOD(1)) | (TPM_SC_PS(7)); // LPTPM counter increments on every LPTPM counter clock
-    TPM1->SC &= ~(TPM_SC_CPWMS_MASK); // up counting mode
+    // Configure TPMs for edge-aligned PWM, prescaler 128
+    TPM0->SC = TPM_SC_CMOD(1) | TPM_SC_PS(7);
+    TPM1->SC = TPM_SC_CMOD(1) | TPM_SC_PS(7);
+    TPM2->SC = TPM_SC_CMOD(1) | TPM_SC_PS(7);
 
-    // Same config as TPM1
-    TPM2->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
-    TPM2->SC |= (TPM_SC_CMOD(1)) | (TPM_SC_PS(7)); 
-    TPM2->SC &= ~(TPM_SC_CPWMS_MASK);
+    TPM0->SC &= ~TPM_SC_CPWMS_MASK;
+    TPM1->SC &= ~TPM_SC_CPWMS_MASK;
+    TPM2->SC &= ~TPM_SC_CPWMS_MASK;
 
-    // Enable PWM on TPM1 Channel 0 -> PTB0
-    TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-    TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
-
-    // Enable PWM on TPM1 Channel 1 -> PTB1
-    TPM1_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-    TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
-
-    // Enable PWM on TPM2 Channel 0 -> PTB3
-    TPM2_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-    TPM2_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
-
-    // Enable PWM on TPM2 Channel 1 -> PTB4
-    TPM2_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-    TPM2_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+    // Enable PWM on each TPM channel
+    TPM1_C0SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // L_BACK_FW
+    TPM1_C1SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // L_BACK_BW
+    TPM2_C0SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // R_BACK_FW
+    TPM2_C1SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // R_BACK_BW
+    TPM0_C1SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // L_FRONT_FW
+    TPM0_C0SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // L_FRONT_BW
+    TPM1_C0SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // R_FRONT_FW
+    TPM1_C1SC = TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1); // R_FRONT_BW
 }
 
-static void delay(volatile uint32_t nof) {
-    while(nof != 0) {
-        __asm("NOP");
-        nof--;
-    }
+void stopAllMotors() {
+    TPM0_C0V = TPM0_C1V = 0;
+    TPM1_C0V = TPM1_C1V = 0;
+    TPM2_C0V = TPM2_C1V = 0;
 }
 
-void stopMotor() {
-    TPM1->MOD = 0;
-    // Captured LPTPM counter value of the input modes
-    TPM1_C0V = 0; // Stop Left motors Fw
-    TPM1_C1V = 0; // Stop Left motors Back
-
-    TPM2->MOD = 0;
-    TPM2_C0V = 0; // Stop Right motors fw
-    TPM2_C1V = 0; // Stop Right motors back
+void forwardAll() {
+    TPM0_C1V = DUTY_CYCLE; // L_FRONT_FW
+    TPM1_C0V = DUTY_CYCLE; // L_BACK_FW
+    TPM2_C0V = DUTY_CYCLE; // R_BACK_FW
+    TPM1_C0V = DUTY_CYCLE; // R_FRONT_FW
 }
 
-void forwardMotor() {
-    TPM1->MOD = DUTY_CYCLE;
-    TPM1_C0V = DUTY_CYCLE; // Left motors forward
-
-    TPM2->MOD = DUTY_CYCLE;
-    TPM2_C0V = DUTY_CYCLE; // Right motors forward
+void reverseAll() {
+    TPM0_C0V = DUTY_CYCLE; // L_FRONT_BW
+    TPM1_C1V = DUTY_CYCLE; // L_BACK_BW
+    TPM2_C1V = DUTY_CYCLE; // R_BACK_BW
+    TPM1_C1V = DUTY_CYCLE; // R_FRONT_BW
 }
 
-void reverseMotor() {
-    TPM1->MOD = DUTY_CYCLE;
-    TPM1_C1V = DUTY_CYCLE; // Left motors reverse
-
-    TPM2->MOD = DUTY_CYCLE;
-    TPM2_C1V = DUTY_CYCLE; // Right motors reverse
+void turnLeft() {
+    TPM0_C0V = DUTY_CYCLE / speed; // L_FRONT_BW
+    TPM1_C1V = DUTY_CYCLE / speed; // L_BACK_BW
+    TPM2_C0V = DUTY_CYCLE / speed; // R_BACK_FW
+    TPM1_C0V = DUTY_CYCLE / speed; // R_FRONT_FW
 }
 
-void leftTurn() {
-    TPM1->MOD = DUTY_CYCLE;
-    TPM1_C1V = DUTY_CYCLE / speed; // Left motors reverse
-
-    TPM2->MOD = DUTY_CYCLE;
-    TPM2_C0V = DUTY_CYCLE / speed; // Right motors forward
+void turnRight() {
+    TPM0_C1V = DUTY_CYCLE / speed; // L_FRONT_FW
+    TPM1_C0V = DUTY_CYCLE / speed; // L_BACK_FW
+    TPM2_C1V = DUTY_CYCLE / speed; // R_BACK_BW
+    TPM1_C1V = DUTY_CYCLE / speed; // R_FRONT_BW
 }
 
-void rightTurn() {
-    TPM1->MOD = DUTY_CYCLE;
-    TPM1_C0V = DUTY_CYCLE / speed; // Left motors forward
-
-    TPM2->MOD = DUTY_CYCLE;
-    TPM2_C1V = DUTY_CYCLE / speed; // Right motors reverse
-}
 
 int main (void) {
     initPWM();
